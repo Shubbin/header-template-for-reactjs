b94637af072ab6d22adae9bd98100f6885e28d84
makinde olasubomi <121644515+Shubbin@users.noreply.github.com>
2024-08-07 23:29:15 +0100

    changes to button

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Button(props) {
   return (
     <Link to={props.to}>
-      <button className='w-195 h-73 px-2 py-2 rounded-[35px] border border-white text-white font-[Poppins] hover:bg-transparent hover:opacity-75 transition-opacity duration-300'>
+      <button className='w-195 h-73 px-2 py-2 rounded-[35px] border border-white text-white font-regular text-[18] font-[Poppins] hover:bg-transparent hover:opacity-75 transition-opacity duration-300'>
         {props.children}
       </button>
     </Link>
